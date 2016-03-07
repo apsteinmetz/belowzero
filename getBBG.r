b34184a5abe571f8d06c600661613e25f70ede32
apsteinmetz <apsteinmetz@yahoo.com>
2016-03-07 12:16:18 -0500

    rebuilding gets sec tickers but not yields

--- getBBG.r
+++ getBBG.r
@@ -4,17 +4,26 @@ library(dplyr)
 
 #specify which maturities to get from the bbg yield curve vector
 curveTenors<-data.frame(bbgColumns=c(3,4,5,6,7,11),tenors=c(1,2,3,4,5,10))
+
 # from excel
 
 #=BDH(BDS($A2,"CURVE_MEMBERS","Dir=h","startrow="&R$1&",endrow="&R$1),"PX_LAST",$M$1,$M$1,"fill=x")
 
 blpConnect()
-curveMembers<-data.frame()
+allSecTickers<-data.frame()
 curveTickers<-read.csv("curve tickers.csv",stringsAsFactors = FALSE)
 for (i in  1:nrow(curveTickers)){
   secTickers<-bds(curveTickers$Ticker[i],"CURVE_MEMBERS")
+  secTickers2<-secTickers[curveTenors$bbgColumns,]
   print (paste(i,curveTickers$Ticker[i],curveTickers$CountryISO[i]))
-  allSecTickers<-rbind(data.frame(Country=curveTickers$CountryISO[i],secTicker=secTickers$'Curve Members'))
+  allSecTickers<-rbind(allSecTickers,data.frame(region=curveTickers$Region[i],
+                                                Country=curveTickers$CountryISO[i],
+                                                tenor=curveTenors$tenors,
+                                                secTicker=secTickers2))
+  
 }
+allSecTickers$secTicker=as.character(allSecTickers$secTicker)
 
-blpDisconnect()
+
+
+#blpDisconnect()
